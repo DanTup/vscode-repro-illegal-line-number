d6c19d67ea5fc5df9355924d4808a0d114fba9a5
Danny Tuppeny <danny@tuppeny.com>
2026-02-24 16:19:10 +0000

    Add example test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,30 @@
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	async function setContent(editor: vscode.TextEditor, content: string): Promise<void> {
+		const doc = editor.document;
+		const all = new vscode.Range(
+			doc.positionAt(0),
+			doc.positionAt(doc.getText().length),
+		);
+
+		await editor.edit((eb) => eb.replace(all, content));
+	}
+
+	function delay(milliseconds: number): Promise<void> {
+		return new Promise((resolve) => setTimeout(resolve, milliseconds));
+	}
+
+	test('Sample test', async () => {
+		const doc = await vscode.workspace.openTextDocument({ content: "" });
+		const editor = await vscode.window.showTextDocument(doc);
+
+		await setContent(editor, "\nfoo() {\n  // print('foo');\n}\n\nbar() {\n  // print('bar');\n}\n\t\t// 1 \n// 1 \n// 1 \n");
+		await delay(1000);
+		await setContent(editor, "0123456789");
 	});
 });
